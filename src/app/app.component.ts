374448d14851dcc28b1d52b2bdfc8f51f4af787f
G <Vyshali.G@elektrobit.com>
2024-10-17 09:35:51 +0530

    admin lte checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { ControlSidebarComponent } from './control-sidebar/control-sidebar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MainHeaderComponent,
+    MainSidebarComponent,
+    ContentWrapperComponent,
+    MainFooterComponent,
+    ControlSidebarComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'dashboard';
+  title = 'apps';
 }
